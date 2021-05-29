7a06a948843657412bc1dc5312f78b9e12dc58c9
Rankuli-Ang <84228020+Rankuli-Ang@users.noreply.github.com>
2021-05-29 10:22:21 +0300

    Update main.py
    Есть проблема с death, сущность проблемы представляю, исправлю несколько позже

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
 import random
 
-# Переменные (я так понимаю что выносить их таким образом не совсем корректно)
-Name = 0
-Health = 0
-Damage = 0
-Defense = 0
-Initiative = 0
-Experience = 0
-Level = 0
 Corpses = 0
 Remains = 0
 Rounds = 0
 number_of_battles = 0
 level_up_variations = ['damage', 'defense', 'initiative']
-level_up_value = 0
-Scourge = 0
 
 # Формирование списка имён червей
-Worms_names_list = ["asd"]
+Worms_names_list = []
+Raw_worms_names_file = open('Names.txt')
+lines = list(Raw_worms_names_file)
+for line in lines:
+    Nms = line[:-1]
+    Worms_names_list.append(Nms)
+
+
 # Корпус червей
 
-
-a = {}
-a = {"id": "a"}
-print(a)
 class Worm:
     def __init__(self):
-        self.name: Name = random.choice(Worms_names_list)
-        self.health: Health = random.randint(6, 12)
-        self.damage: Damage = random.randint(1, 3)
-        self.defense: Defense = random.uniform(0.5, 1.0)
-        self.initiative: Initiative = random.randint(1, 10)
-        self.experience: Experience = 0
-        self.level: Level = 1
+        self.name: str = random.choice(Worms_names_list)
+        self.health: int = random.randint(6, 12)
+        self.damage: int = random.randint(1, 3)
+        self.defense: float = random.uniform(0.5, 1.0)
+        self.initiative: int = random.randint(1, 10)
+        self.experience: int = 0
+        self.level: int = 1
+        self.poisoned: int = 0
 
-    def describe(self) -> None:
+    def describe(self):
         print(f'Worm {self.name}:')
         print(f'\thealth {self.health}')
         print(f'\tdamage {self.damage}')
@@ -73,13 +67,14 @@ class Worm:
         if self.health <= 0:
             global Corpses
             Corpses += 1
-
             return worms.remove(self)
-    def regeneration(self):
+
+    def poisoning(self, other):
         pass
 
     def strike(self, other):
         other.health = other.health - self.damage * other.defense
+        self.poisoning(other)
         return other.health
 
     def attack(self, other):
@@ -109,23 +104,59 @@ class Worm:
             other.level_up()
             self.death()
 
-            self.strike (other)
+            self.strike(other)
             self.experience += 1
             self.level_up()
             other.death()
 
 
-class Hydralisk(Worm):
+class Defiler(Worm):
     def __init__(self):
-        super(Hydralisk, self).__init__()
-
-    def regeneration(self):
-        self.health += 2
-        return self.health
+        super(Defiler, self).__init__()
 
     def death(self):
-        self.regeneration()
-        super(Hydralisk, self).death()
+        if self.health <= 0:
+            global Corpses
+            Corpses += 1
+            worms.append(Zerling())
+            worms.append(Zerling())
+            worms.append(Zerling())
+            worms.append(Zerling())
+            worms.append(Zerling())
+            return worms.remove(self)
+
+
+class Zerling(Worm):
+    def __init__(self):
+        super(Zerling, self).__init__()
+        self.health: int = random.randint(3, 5)
+        self.damage: int = random.randint(1, 2)
+        self.defense: float = random.uniform(0.8, 1.0)
+        self.initiative: int = random.randint(5, 10)
+        self.experience: int = 2
+        self.level: int = 1
+        self.poisoned: int = 0
+
+
+class Infectoid(Worm):
+    def __init__(self):
+        super(Infectoid, self).__init__()
+
+    def poisoning(self, other):
+        other.poisoned += 3
+        return other.poisoned
+
+    def strike(self, other):
+        other.health = other.health - self.damage
+        self.poisoning(other)
+        return other.health
+
+    def death(self):
+        if self.health <= 0:
+            global Corpses
+            Corpses += 1
+            worms.remove(random.choice(worms))
+            return worms.remove(self)
 
 
 def corpsegrinding():
@@ -144,32 +175,38 @@ def corpsegrinding():
     Rounds = 0
 
 
-
-
-
 # Тело программы
 if __name__ == "__main__":
-    worms = [Worm() for i in range(1, 51)] + [Hydralisk()]
+    Defilers_list = [Defiler() for i in range(1, 11)]
+    Zerlings_list = [Zerling() for i in range(1, 21)]
+    Infectoids_list = [Infectoid() for i in range(1, 6)]
+    worms = Defilers_list + Zerlings_list + Infectoids_list
 
     while len(worms) > 1:
+        for unit in worms:
+            if unit.poisoned > 0:
+                unit.health -= 1
+                unit.death()
         warrior1 = random.choice(worms)
         warrior2 = random.choice(worms)
         if warrior1 != warrior2:
             warrior1.attack(warrior2)
 
-
     print('Number of battles:')
     print(number_of_battles)
 
+    Number_of_Battles_file = open('Number_of_Battles.txt').read()
+    Number_of_Battles_raw = int(Number_of_Battles_file) + number_of_battles
 
     print('Total Number of battles:')
+    print(Number_of_Battles_raw)
+
+    Number_of_Battles_file = open('Number_of_Battles.txt', 'w')
+    Number_of_Battles_file.write(str(Number_of_Battles_raw))
+    Number_of_Battles_file.close()
 
     if len(worms) == 1:
         print(worms[0].describe())
-        worms.append(Hydralisk())
-        worms[1].describe()
+        print(type(worms[0]))
     else:
         print('All is dead!')
-
-
-
