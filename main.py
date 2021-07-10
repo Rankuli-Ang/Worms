85173cfe7505e2066dda99642e884c9a8676a5cb
Rankuli-Ang <84228020+Rankuli-Ang@users.noreply.github.com>
2021-07-10 12:18:01 +0300

    update main 10.07.21
    refactored version of main

--- main.py
+++ main.py
@@ -1,212 +1,144 @@
 import random
+from typing import List
 
-Corpses = 0
-Remains = 0
-Rounds = 0
-number_of_battles = 0
-level_up_variations = ['damage', 'defense', 'initiative']
+import cv2
+import numpy as np
 
-# Формирование списка имён червей
-Worms_names_list = []
-Raw_worms_names_file = open('Names.txt')
-lines = list(Raw_worms_names_file)
-for line in lines:
-    Nms = line[:-1]
-    Worms_names_list.append(Nms)
+from worm import Role, Worm, Food
 
 
-# Корпус червей
+# from opencv import * # плохой тон
 
-class Worm:
+# import opencv
+# class Home(opencv.Role)
+
+class Home(Role):
     def __init__(self):
-        self.name: str = random.choice(Worms_names_list)
-        self.health: int = random.randint(6, 12)
-        self.damage: int = random.randint(1, 3)
-        self.defense: float = random.uniform(0.5, 1.0)
-        self.initiative: int = random.randint(1, 10)
-        self.experience: int = 0
-        self.level: int = 1
-        self.poisoned: int = 0
+        super().__init__(0, 0)
 
-    def describe(self):
-        print(f'Worm {self.name}:')
-        print(f'\thealth {self.health}')
-        print(f'\tdamage {self.damage}')
-        print(f'\tdefense {self.defense}')
-        print(f'\tinitiative {self.initiative}')
-        print(f'\tlevel {self.level}')
-        print(f'\texperience {self.experience}')
 
-    def level_up(self):
-        if self.experience >= self.level + 3:
-            self.level += 1
-            self.experience = 0
-            level_up_value = random.choice(level_up_variations)
-            if level_up_value == level_up_variations[0]:
-                self.level_up_damage()
-            elif level_up_value == level_up_variations[1]:
-                self.level_up_defense()
-            else:
-                self.level_up_initiative()
+class World:
+    def __init__(self):
+        self.worms: List[Worm] = []
+        self.food: List[Food] = []
+        self.name: str = 'World'
 
-    def level_up_damage(self):
-        self.damage = self.level + 2
-        self.health = self.level + 5
+        self.height = 100
+        self.width = 100
 
-    def level_up_defense(self):
-        self.defense -= self.level / 200 + 0.05
-        if self.defense < 0.2:
-            self.defense = 0.2
-        self.health = self.level + 5
+    @property
+    def worms_by_initiative(self) -> List[Worm]:
+        worms_by_initiative = world.worms
+        worms_by_initiative.sort(key=lambda worm: worm.initiative)
+        return worms_by_initiative
 
-    def level_up_initiative(self):
-        self.initiative = self.level + 1
-        self.health = self.level + 5
+    def worms_at(self, x: int, y: int) -> List[Worm]:
+        return [worm for worm in self.worms if worm.coordinate_y == y and worm.coordinate_x == x]
 
-    def death(self):
-        if self.health <= 0:
-            global Corpses
-            Corpses += 1
-            return worms.remove(self)
 
-    def poisoning(self, other):
+def populate_world(world: World, worms_num: int = 100) -> None:
+    for i in range(worms_num):
+        worm = Worm(random.randint(0, world.width), random.randint(0, world.height))
+        world.worms.append(worm)
+
+
+class WorldProcessor:
+    def __init__(self):
         pass
 
-    def strike(self, other):
-        other.health = other.health - self.damage * other.defense
-        self.poisoning(other)
-        return other.health
-
-    def attack(self, other):
-        global number_of_battles
-        number_of_battles = number_of_battles + 1
-        global Rounds
-        Rounds += 1
-        if Rounds >= 10:
-            if Corpses >= 1:
-                corpsegrinding()
-        if self.initiative >= other.initiative:
-
-            self.strike(other)
-            self.experience += 1
-            self.level_up()
-            other.death()
-
-            other.strike(self)
-            other.experience += 1
-            other.level_up()
-            self.death()
-
-        else:
-
-            other.strike(self)
-            other.experience += 1
-            other.level_up()
-            self.death()
-
-            self.strike(other)
-            self.experience += 1
-            self.level_up()
-            other.death()
+    def process(self, world: World) -> None:
+        pass
 
 
-class Defiler(Worm):
+class Visualizer(WorldProcessor):
     def __init__(self):
-        super(Defiler, self).__init__()
+        super(Visualizer, self).__init__()
 
-    def death(self):
-        if self.health <= 0:
-            global Corpses
-            Corpses += 1
-            worms.append(Zerling())
-            worms.append(Zerling())
-            worms.append(Zerling())
-            worms.append(Zerling())
-            worms.append(Zerling())
-            return worms.remove(self)
+        self._color_worm = (255, 255, 255)
+        self._color_space = (0, 0, 0)
+        self._color_food = (0, 0, 255)
+
+    def process(self, world: World) -> None:
+        vis = np.zeros((world.height, world.width, 3), dtype='uint8')
+
+        for worm in world.worms:
+            vis[worm.coordinate_y, worm.coordinate_x] = self._color_worm
+
+        scale = 4
+        vis = cv2.resize(vis, None, fx=scale, fy=scale, interpolation=cv2.INTER_NEAREST)
+
+        cv2.imshow('vis', vis)
+        cv2.waitKey(1)
 
 
-class Zerling(Worm):
+class MovementProcessor(WorldProcessor):
     def __init__(self):
-        super(Zerling, self).__init__()
-        self.health: int = random.randint(3, 5)
-        self.damage: int = random.randint(1, 2)
-        self.defense: float = random.uniform(0.8, 1.0)
-        self.initiative: int = random.randint(5, 10)
-        self.experience: int = 2
-        self.level: int = 1
-        self.poisoned: int = 0
+        super(MovementProcessor, self).__init__()
+
+    def process(self, world: World) -> None:
+        for worm in world.worms_by_initiative:
+            movements = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            dcoord = random.choice(movements)
+            worm.move(dcoord[0], dcoord[1], world.width, world.height)
 
 
-class Infectoid(Worm):
+class FightProcessor(WorldProcessor):
     def __init__(self):
-        super(Infectoid, self).__init__()
+        super(FightProcessor, self).__init__()
 
-    def poisoning(self, other):
-        other.poisoned += 3
-        return other.poisoned
+    def process(self, world: World) -> None:
+        for worm in world.worms_by_initiative:
+            targets = world.worms_at(worm.coordinate_x, worm.coordinate_y)
+            target = random.choice(targets)
 
-    def strike(self, other):
-        other.health = other.health - self.damage
-        self.poisoning(other)
-        return other.health
+            if target is worm:
+                continue
 
-    def death(self):
-        if self.health <= 0:
-            global Corpses
-            Corpses += 1
-            worms.remove(random.choice(worms))
-            return worms.remove(self)
+            worm.strike(target)
+            target.strike(worm)
 
 
-def corpsegrinding():
-    global Remains
-    global Rounds
-    Remains = Corpses / len(worms)
+class LevelUpProcessor(WorldProcessor):
+    def __init__(self):
+        super(LevelUpProcessor, self).__init__()
 
-    def feeding():
-        nonlocal eater
-        eater.health = eater.health + Remains
-        return eater.health
-
-    for eater in worms:
-        feeding()
-    Remains = 0
-    Rounds = 0
+    def process(self, world: World) -> None:
+        for worm in world.worms:
+            worm.level_up()
 
 
-# Тело программы
+class CorpseGrindingProcessor(WorldProcessor):
+    def __init__(self):
+        super(CorpseGrindingProcessor, self).__init__()
+
+    def process(self, world: World) -> None:
+        for worm in world.worms:
+            if not worm.dead:
+                continue
+
+            eaters = world.worms_at(worm.coordinate_x, worm.coordinate_y)
+            for eater in eaters:
+                if eater.dead:
+                    continue
+                eater.health += worm.level
+
+
+class DeadWormsRemover(WorldProcessor):
+    def __init__(self):
+        super(DeadWormsRemover, self).__init__()
+
+    def process(self, world: World) -> None:
+        alive_worms = [worm for worm in world.worms if not worm.dead]
+        world.worms = alive_worms
+
+
 if __name__ == "__main__":
-    Defilers_list = [Defiler() for i in range(1, 11)]
-    Zerlings_list = [Zerling() for i in range(1, 21)]
-    Infectoids_list = [Infectoid() for i in range(1, 6)]
-    worms = Defilers_list + Zerlings_list + Infectoids_list
+    world = World()
+    populate_world(world, 1000)
 
-    while len(worms) > 1:
-        for unit in worms:
-            if unit.poisoned > 0:
-                unit.health -= 1
-                unit.death()
-        warrior1 = random.choice(worms)
-        warrior2 = random.choice(worms)
-        if warrior1 != warrior2:
-            warrior1.attack(warrior2)
+    processors = [MovementProcessor(), FightProcessor(), CorpseGrindingProcessor(), DeadWormsRemover(),
+                  LevelUpProcessor(), Visualizer()]
 
-    print('Number of battles:')
-    print(number_of_battles)
-
-    Number_of_Battles_file = open('Number_of_Battles.txt').read()
-    Number_of_Battles_raw = int(Number_of_Battles_file) + number_of_battles
-
-    print('Total Number of battles:')
-    print(Number_of_Battles_raw)
-
-    Number_of_Battles_file = open('Number_of_Battles.txt', 'w')
-    Number_of_Battles_file.write(str(Number_of_Battles_raw))
-    Number_of_Battles_file.close()
-
-    if len(worms) == 1:
-        print(worms[0].describe())
-        print(type(worms[0]))
-    else:
-        print('All is dead!')
+    while True:
+        for proc in processors:
+            proc.process(world)
