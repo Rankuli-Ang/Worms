f250c892993c8334f75e2169b35a45685e53b5e3
Rankuli-Ang <84228020+Rankuli-Ang@users.noreply.github.com>
2021-07-10 13:53:21 +0300

    Add food

--- main.py
+++ main.py
@@ -35,11 +35,20 @@ class World:
     def worms_at(self, x: int, y: int) -> List[Worm]:
         return [worm for worm in self.worms if worm.coordinate_y == y and worm.coordinate_x == x]
 
+    def food_at(self, x: int, y: int) -> List[Food]:
+        return [food_unit for food_unit in self.food if food_unit.coordinate_x == x and food_unit.coordinate_y == y]
+
 
 def populate_world(world: World, worms_num: int = 100) -> None:
     for i in range(worms_num):
         worm = Worm(random.randint(0, world.width), random.randint(0, world.height))
         world.worms.append(worm)
+
+
+def sow_food(world: World, food_num: int = 10) -> None:
+    for i in range(food_num):
+        food_unit = Food(random.randint(0, world.width), random.randint(0, world.height))
+        world.food.append(food_unit)
 
 
 class WorldProcessor:
@@ -63,6 +72,9 @@ class Visualizer(WorldProcessor):
 
         for worm in world.worms:
             vis[worm.coordinate_y, worm.coordinate_x] = self._color_worm
+
+        for food_unit in world.food:
+            vis[food_unit.coordinate_y - 1, food_unit.coordinate_x - 1] = self._color_food  # непонятная проблема
 
         scale = 4
         vis = cv2.resize(vis, None, fx=scale, fy=scale, interpolation=cv2.INTER_NEAREST)
@@ -107,6 +119,27 @@ class LevelUpProcessor(WorldProcessor):
             worm.level_up()
 
 
+class FoodPickUpProcessor(WorldProcessor):
+    def __init__(self):
+        super(FoodPickUpProcessor, self).__init__()
+
+    def process(self, world: World) -> None:
+        for eater in world.worms_by_initiative:
+            targets = world.food_at(eater.coordinate_x, eater.coordinate_y)
+            if len(targets) != 0:
+                target = random.choice(targets)
+                eater.eat(target)
+
+
+class EatenFoodRemover(WorldProcessor):
+    def __init__(self):
+        super(EatenFoodRemover, self).__init__()
+
+    def process(self, world: World) -> None:
+        leftover_food = [food_unit for food_unit in world.food if not food_unit.eaten]
+        world.food = leftover_food
+
+
 class CorpseGrindingProcessor(WorldProcessor):
     def __init__(self):
         super(CorpseGrindingProcessor, self).__init__()
@@ -135,9 +168,10 @@ class DeadWormsRemover(WorldProcessor):
 if __name__ == "__main__":
     world = World()
     populate_world(world, 1000)
+    sow_food(world, 1000)
 
-    processors = [MovementProcessor(), FightProcessor(), CorpseGrindingProcessor(), DeadWormsRemover(),
-                  LevelUpProcessor(), Visualizer()]
+    processors = [MovementProcessor(), FightProcessor(), CorpseGrindingProcessor(), FoodPickUpProcessor(),
+                  DeadWormsRemover(), EatenFoodRemover(), LevelUpProcessor(), Visualizer()]
 
     while True:
         for proc in processors:
