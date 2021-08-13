6a418d20d3eefc1decb6830aae1fd3c010e9c20d
Rankuli-Ang <84228020+Rankuli-Ang@users.noreply.github.com>
2021-08-13 21:37:58 +0300

    21.08.13 - tornado_scatter add

--- common_types.py
+++ common_types.py
@@ -10,6 +10,7 @@ class Colors(Enum):
     GREEN = (0, 255, 0)
     YELLOW = (0, 255, 255)
     SKY_BLUE = (235, 206, 135)
+    GREY = (72, 72, 72)
 
 
 Cell = namedtuple('Cell', ['x', 'y'])
@@ -22,4 +23,15 @@ class Neighbors(Enum):
     LEFT = Cell(-1, 0)
 
 
-Neighbors_values = [Neighbors.UP.value, Neighbors.DOWN.value, Neighbors.LEFT.value, Neighbors.RIGHT.value]
+class Tornado_scatter(Enum):
+    UP = Cell(0, -5)
+    RIGHT = Cell(5, 0)
+    DOWN = Cell(0, 5)
+    LEFT = Cell(-5, 0)
+
+
+neighbors_values = [Neighbors.UP.value, Neighbors.DOWN.value, Neighbors.LEFT.value, Neighbors.RIGHT.value]
+
+tornado_scatter_values = [Tornado_scatter.RIGHT.value, Tornado_scatter.LEFT.value, Tornado_scatter.DOWN.value,
+                          Tornado_scatter.UP.value]
+
