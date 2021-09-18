f023ed38a45e33e6c406b2129b9585e9443d75f3
Rankuli-Ang <84228020+Rankuli-Ang@users.noreply.github.com>
2021-09-18 12:44:27 +0300

    21.09.18 - pylint corrections added

--- common_types.py
+++ common_types.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 
 
 class Colors(Enum):
+    """Displayed colors of objects on the map."""
     SPACE = (0, 0, 0)
     FOOD = (0, 0, 255)
     WHITE = (255, 255, 255)
@@ -17,29 +18,33 @@ Cell = namedtuple('Cell', ['x', 'y'])
 
 
 class Neighbors(Enum):
+    """Nearest cells for interaction and movement of objects."""
     UP = Cell(0, -1)
     RIGHT = Cell(1, 0)
     DOWN = Cell(0, 1)
     LEFT = Cell(-1, 0)
 
 
-class Tornado_scatter(Enum):
+class TornadoScatter(Enum):
+    """Options for moving objects when hitting the tornado affected area."""
     UP = Cell(0, -5)
     RIGHT = Cell(5, 0)
     DOWN = Cell(0, 5)
     LEFT = Cell(-5, 0)
 
 
-NEIGHBOURS_VALUES = [Neighbors.UP.value, Neighbors.DOWN.value, Neighbors.LEFT.value, Neighbors.RIGHT.value]
+NEIGHBOURS_VALUES = [Neighbors.UP.value, Neighbors.DOWN.value,
+                     Neighbors.LEFT.value, Neighbors.RIGHT.value]
 
-tornado_scatter_values = [Tornado_scatter.RIGHT.value, Tornado_scatter.LEFT.value, Tornado_scatter.DOWN.value,
-                          Tornado_scatter.UP.value]
+tornado_scatter_values = [TornadoScatter.RIGHT.value, TornadoScatter.LEFT.value,
+                          TornadoScatter.DOWN.value, TornadoScatter.UP.value]
 
 
 if __name__ == "__main__":
+    # I will delete it after cell corrections
     import time
     c1, c2 = Cell(10, 10), Cell(20, 20)
-    p1, p2 = [10, 10], [20, 20]
+    p1, p2 = (10, 10), (20, 20)
     iters = 100000
     t1 = time.time()
     for i in range(iters):
