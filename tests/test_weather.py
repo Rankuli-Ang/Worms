72fa2031ec490ddcb51966e126008eeaa59578e2
Vladislav Prosvetov <84228020+Rankuli-Ang@users.noreply.github.com>
2021-11-08 23:37:37 +0300

    Update test_weather.py

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,6 +1,6 @@
 import unittest
-from active_characters import weather
-from active_characters import worm
+from Worms.src import weather
+from Worms.src.active_characters import worm
 
 
 class TestWeather(unittest.TestCase):
